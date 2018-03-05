5d1073aa441f218141e607f28e5675a27636ab6d
FluidCoding <sonnie130@gmail.com>
2018-03-05 17:03:56 -0500

    AC

--- main.js
+++ main.js
@@ -1,6 +1,58 @@
-var app6 = new Vue({
-    el: '#app-6',
+var appSQLTranslate = new Vue({
+    el: '#md-form',
     data: {
-      message: 'Hello Vue!'
+      sqlVarName: 'SQLStr',
+      preseveNewLine: true,
+      sqlRaw: '',
+      sqlText: ''
+    },
+    methods: {
+      transformSQLStr: function(event){
+        //& vbCrLf
+        const lnEnd = (this.preseveNewLine?" & vbCrLf" : "");
+        // let sql = this.sqlRaw;
+        let sql = '';
+        sql = this.sqlVarName + ' = \"';
+        
+        // sql = this.sqlVarName + ' = \"' + this.sqlRaw;
+        // sql = sql.replace(  ,   )
+
+        // Replace Variables 
+          // string type
+          // sql = sql.replace(/\{'(\w+)\}'/gm, "\" & $1 \" & \"'"  );
+
+          // sql = sql.replace(/\{(\w+)\}/g, "\" & $1 "  );
+
+        const lines = this.sqlRaw.split(/\n/);
+        // console.log(lines);
+        lines.forEach( (s,i) => {
+          if(i>0) sql = sql + '"'+lnEnd+'\n'+this.sqlVarName+' = '+ this.sqlVarName +' & \"';
+          sql = sql + s.replace(/\{(\w+)\}/g, "\" & $1 & \""  ) + "\"";
+          
+        })
+
+        // sql = sql.replace(/\{(\w+)\}/g, function( match, p1,p2) {
+          
+        //   return p1 + '';
+
+        // } );
+
+        // sql = sql.replace(/\n/g, '"'+lnEnd+'\n'+this.sqlVarName+' = '+ this.sqlVarName +' & \"'  )
+        this.sqlText = sql;
+        
+        console.log('transforming sql...', this.sqlRaw)
+      }
     }
   })
+
+  /*
+
+
+select * from users 
+where instit_id = {I_ID}
+and role = '{U_ROLE}'
+
+
+  select * from users 
+where instit_id = {I_ID}
+*/
