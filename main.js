ed0dbf2d1996df35056a8d0de0c509b61d1da07e
= <=>
2018-07-24 13:49:15 -0400

    Fixed, supporting non-word only variables, i.e. Arrays

--- main.js
+++ main.js
@@ -9,29 +9,19 @@ var appSQLTranslate = new Vue({
     },
     methods: {
       transformSQLStr: function(event){
-        //& vbCrLf
         const lnEnd = (this.sqlNewLinesOn?" & vbCrLf" : "");
         let sql = '';
         sql = this.sqlVarName + ' = \"';
-        // Replace Variables 
-          // string type
-          // sql = sql.replace(/\{'(\w+)\}'/gm, "\" & $1 \" & \"'"  );
-          // sql = sql.replace(/\{(\w+)\}/g, "\" & $1 "  );
-
         const lines = this.sqlRaw.split(/\n/);
         lines.forEach( (s,i) => {
           if(i>0) sql = sql + '"'+lnEnd+'\n'+this.sqlVarName+' = '+ this.sqlVarName +' & \"';
-          sql = sql + s.replace(/\{(\w+)\}/g, "\" & $1 & \""  );
+          sql = sql + s.replace(/\{([^{}]+)\}/g, "\" & $1 & \""  );
         })
         sql = sql + '"'+lnEnd;
         this.sqlText = sql;
 
         $('#sqlOutSyntax').text(this.sqlRaw);
-        $('#sqlOutSyntax').each(function(i, block) {
-          hljs.highlightBlock(block);
-        });
-        // this.sqlSyntax = $('#sqlOutSyntax').html();
-        // $('#sqlOutSyntax2').html($('#sqlOutSyntax').html());
+        $('#sqlOutSyntax').each( (i, block) => hljs.highlightBlock(block));
       }
     }
   })
