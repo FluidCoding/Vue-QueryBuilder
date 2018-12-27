4ebb7ec8f1908660153fde7546a6f05c7d95b23a
FluidCoding <sonnie130@gmail.com>
2018-12-27 04:04:18 -0500

    Added Option for Parameterized Query Building

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const appSQLTranslate = new Vue({
     sqlRaw: "",
     sqlText: "",
     sqlSyntax: "",
-    theme: "dark"
+    theme: "dark",
+    sqlParamQueryOn: false,
+    sqlParams: []
   },
   methods: {
+    callTransform(t) {
+      if (this.sqlParamQueryOn) this.transformSQLParamStr();
+      else this.transformSQLStr();
+    },
     transformSQLStr() {
       const lnEnd = this.sqlNewLinesOn ? " & vbCrLf" : "";
       let sql = "";
@@ -23,6 +29,30 @@ const appSQLTranslate = new Vue({
         sql = sql + s.replace(/\{([^{}]+)\}/g, '" & $1 & "');
       });
       sql = sql + '"' + lnEnd;
+      this.sqlText = sql;
+
+      $("#sqlOutSyntax").text(this.sqlRaw);
+      $("#sqlOutSyntax").each((i, block) => hljs.highlightBlock(block));
+    },
+    transformSQLParamStr() {
+      const lnEnd = this.sqlNewLinesOn ? " & vbCrLf" : "";
+      let sql = "";
+      sql = this.sqlVarName + ' = "';
+      const lines = this.sqlRaw.split(/\n/);
+      let params = "";
+      lines.forEach((s, i) => {
+        if (i > 0) {
+          if (this.sqlSingleLineOn) s = " " + s + params;
+          else sql = sql + '"' + lnEnd + params + "\n" + this.sqlVarName + " = " + this.sqlVarName + ' & "';
+        }
+        params = (s.match(/\?:{(\s*[^{}]+\s*)}/g) || []).join("").replace(/\?:{([^{}]+)\}/g, " :sqlParam($1) ");
+        s = s.replace(/\?:{(\s*[^{}]+\s*)}/g, "? ");
+        if (s.match(/\{([^{}]+)\}/g) !== null) {
+          s = s.replace(/\{([^{}]+)\}/g, '" & $1 & "');
+        }
+        sql += s;
+      });
+      sql = sql + "" + lnEnd;
       this.sqlText = sql;
 
       $("#sqlOutSyntax").text(this.sqlRaw);
